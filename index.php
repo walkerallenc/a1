cfdaa373aa44574c24e38bf05bb52d109c682091
Allen <walkerallen@comcast.net>
2017-02-12 16:28:36 -0500

    updated personal blub

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <title>Assignment 1</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="acw.css"> 
+    <style type="text/css">
+    @import URL (http://localhost/acw.css);
+    </style> 
+</head>
+<body>
+<div class="h_one">
+<h1>Greetings!</h1>
+</div>
+<br />
+
+<img src='images/acw.jpg' alt='Christmas'>
+<div class="h_two">
+<h2>My name is Allen C. Walker</h2>
+</div>
+<br />
+
+<div>
+<p class="default1">I am a software developer working for a telecommunications company and I live on Cape Cod, Massachusetts. <br />My work experience has primarily been with Microsoft technologies such as C#, VB.net, VB6 & SQL Server.</p>
+<p class="default1">For the last few years I've been supporting a project that consisted heavily of Oracle development and <br /> I'm currently designing and developing a dynamic C#/Oracle web application that will provide a GUI for the <br /> back end processes that have been developed.</p>
+<p> I hope to to gain some knowledge from this class to compliment my work experience in the area of dynamic web application development and also become familiar with tools used with cloud application development.</p>   
+</div>
+</body>
+
+</html>

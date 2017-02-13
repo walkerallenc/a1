52a6a63ce27f7ea15da0a2f45dec257583d04fce
Allen <walkerallen@comcast.net>
2017-02-13 01:19:47 -0500

    updated personal blub,css & html.

--- index.php
+++ index.php
@@ -10,17 +10,22 @@
   </div>
   <br />
 
-  <img src='images/acw.jpg' alt='Christmas'>
+  <div class="h_one">
+    <img src='images/acw.jpg' alt='Christmas'>
+  </div>
   <div class="h_two">
     <h2>My name is Allen C. Walker</h2>
   </div>
   <br />
 
+<tr>
+<td>
 <div>
-  <p class="default1">I am a software developer working for a telecommunications company and I live on Cape Cod, Massachusetts. <br>My work experience has primarily been with Microsoft technologies such as C#, VB.net, VB6 & SQL Server.</p>
-  <p class="default1">For the last few years I've been supporting a project that consisted heavily of Oracle development and <br> I'm currently designing and developing a dynamic C#/Oracle web application that will provide a GUI for the <br> back end processes that have been developed.</p>
-  <p> I hope to to gain some knowledge from this class to compliment my work experience in the area of dynamic web application development.</p>   
+  <p class="default1">I am a software developer working for a telecommunications company and I live on Cape Cod, Massachusetts.<br><br>My work experience has primarily been with Microsoft technologies such as C#, VB.net, VB6 & SQL Server.  For the last few years I've been supporting a project that <br>consists heavily of Oracle development and I'm currently designing and developing a dynamic C# web application that will provide a GUI for the back end processes<br>that have been developed.<br><br>
+  I hope to to gain some knowledge from this class to compliment my work experience in the area of<br>dynamic web application development.</p>   
 </div>
+</td>
+</tr>
 <?php
    /////////////////////////////////////////////////////////////
    // reviewed sample code from sitepoint.com forum community //
@@ -30,11 +35,11 @@
                                   "Our deepest fear is that we are powerful beyond measure.");
     shuffle($aryQuotes);
 ?>
-  <p>
+  <div class="randomquote">
     <b>
        <?php echo $aryQuotes[0]; ?>
     </b>
-  </p>
+  </div>
 </body>
 
 </html>

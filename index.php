a9b28dda34bb6fec756b2194dc40f8b1dbaf448c
Allen <walkerallen@comcast.net>
2017-05-24 09:31:44 -0400

    updated ACW work career summary.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Assignment 1</title>
+    <title>Allen C. Walker</title>
+    <!--<title>Assignment 1</title>-->
     <link rel="stylesheet" type="text/css" href="/css/acw.css" /> 
 </head>
 <body>
@@ -19,8 +20,10 @@
   <br />
 
 <div>
-  <p class="default1">I am a software developer working for a telecommunications company and I live on Cape Cod, Massachusetts.<br><br>My work experience has primarily been with Microsoft technologies such as C#, VB.net, VB6 & SQL Server.  For the last few years I've been supporting a project that <br>consists heavily of Oracle development and I'm currently designing and developing a dynamic C# web application that will provide a GUI for the back end processes<br>that have been developed.<br><br>
-  I hope to gain some knowledge from this class to compliment my work experience in the area of<br>dynamic web application development.</p>   
+  <p class="default1">I am a software developer working for a telecommunications company and I live on Cape Cod, Massachusetts.<br><br>I am also a
+  Harvard Extension School graduate student striving to learn the technologies that will best facilitate meeting the technical challenges of our ever evolving business environment.    
+<br><br>My work experience has primarily been with Microsoft technologies such as C#, VB.net, VB6 & SQL Server.  For the last few years I've been supporting a project that <br>consists heavily of Oracle development and I'm currently designing and developing a dynamic C# web application that will provide a GUI for the back end processes<br>that have been developed.<br><br>
+  I hope to gain knowledge from Harvard Extension School to compliment my work experience in the area of<br>dynamic web application development.</p>   
 </div>
 <?php
    /////////////////////////////////////////////////////////////
